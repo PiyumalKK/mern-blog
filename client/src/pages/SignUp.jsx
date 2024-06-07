c824b294ac7d28aa0fa96e849a68da5b656afa93
PiyumalKK <piyumalm912@gmail.com>
2024-06-07 15:28:39 +0530

    Add functionality to sign up

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,62 @@
 /* eslint-disable react/no-unescaped-entities */
-import { Button, Label, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 //import React from 'react'
 function SignUp() {
+
+  const [formData, setformData] =useState({});
+
+  const [errorMessage,setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  
+  const navigate = useNavigate();
+
+
+  
+
+  const handleChange = (e)=>{
+    //console.log(e.target.value);
+    setformData( {...formData, [e.target.id]: e.target.value.trim()})
+  };
+  //console.log(formData);
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    
+    if(!formData.username || !formData.email || !formData.password )
+      {
+        return setErrorMessage('Please fill out all required fields');
+      }
+
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup',
+        {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(formData),
+        });
+      // eslint-disable-next-line no-unused-vars
+      const data =await res.json();
+
+      if(data.success === false)
+        {
+          return setErrorMessage(data.message);
+        }
+      setLoading(false);
+
+    if(res.ok)
+      {
+        navigate('/sign-in');
+      }
+    
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-8">
@@ -19,21 +72,21 @@ function SignUp() {
         </div>
         <div className="flex-1">  {/*Equal space*/}
         {/*right*/}
-        <form className="flex flex-col gap-4">
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
           <div className="">
             <Label value='Your Username'/>
             <TextInput 
             type="text"
             placeholder="Username"
-            id="username"
+            id="username"onChange={handleChange}
             />
           </div>
           <div className="">
             <Label value='Your Email'/>
             <TextInput 
-            type="text"
+            type="email"
             placeholder="name@example.com"
-            id="email"
+            id="email" onChange={handleChange}
             />
           </div>
           <div className="">
@@ -41,11 +94,23 @@ function SignUp() {
             <TextInput 
             type="text"
             placeholder="Password"
-            id="password"
+            id="password"onChange={handleChange}
             />
           </div>
-          <Button gradientDuoTone='purpleToPink' type='submit'>
-            Sign Up
+          <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+            {/*Sign Up*/}
+            {
+              loading ? (
+                <>
+                <Spinner size ='sm'/>
+                <span className="pl-3">
+                  Loading....
+                </span>
+                </>
+                // as we are adding more than one html element inside the bracket we need to use <></>
+                
+              ) : 'Sign Up'
+            }
           </Button>
         </form>
         <div className="flex gap-2 text-sm mt-5">
@@ -56,6 +121,13 @@ function SignUp() {
           Sign In
           </Link>
         </div>
+        {
+          errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )
+        }
         </div>
       </div>
     </div>
