0472418be3bf2e7604c837dc7674538299a0cf23
PiyumalKK <piyumalm912@gmail.com>
2024-06-12 22:54:21 +0530

    Complete Profile Page UI

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,4 +1,49 @@
 //import React from 'react'
+import { Sidebar } from 'flowbite-react';
+import { HiArrowSmRight, HiUser } from 'react-icons/hi';
+import { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+export default function DashSidebar() {
+  const location = useLocation();
+  // eslint-disable-next-line no-unused-vars
+  const [tab, setTab] = useState('');
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    // console.log(tabFromUrl);
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+
+  return (
+    <Sidebar className='w-full md:w-56'>
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Sidebar.Item
+            as={Link}
+            to='/dashboard?tab=profile'
+            active={tab === 'profile'}
+            icon={HiUser}
+            label={"User"}
+            labelColor='dark'
+          >
+            Profile
+          </Sidebar.Item>
+          <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer'>
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+}
+
+{/*
+  
+//import React from 'react'
 import { Sidebar} from 'flowbite-react';
 import { HiArrowSmRight, HiUser } from 'react-icons/hi';
 import { useEffect,useState } from 'react';
@@ -35,3 +80,4 @@ if(tabFromUrl)
     </Sidebar>
   )
 }
+ */}
