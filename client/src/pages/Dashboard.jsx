65c4c90a065bc3f2d92513c528f25522b2157fb7
PiyumalKK <piyumalm912@gmail.com>
2024-06-12 22:11:35 +0530

    Complete the sidebar of the dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,34 @@
 //import React from 'react'
+import { useEffect, useState } from 'react';
+import {useLocation} from 'react-router-dom';
+import DashSidebar from '../components/DashSidebar';
+import DashProfile from '../components/DashProfile';
 
 function Dashboard() {
+
+const location = useLocation();
+// eslint-disable-next-line no-unused-vars
+const [tab,setTab]=useState('');
+useEffect(()=>{
+const urlParams = new URLSearchParams(location.search);
+const tabFromUrl = urlParams.get('tab');
+//console.log(tabFromUrl);
+if(tabFromUrl)
+  {
+    setTab(tabFromUrl);
+  }
+
+}, [location.search]);
+
   return (
-    <div>Dashboard</div>
+    <div className="min-h-screen flex flex-col md:flex-row">
+       <div className='md:w-56'>
+        {/*Sidebar*/}
+        <DashSidebar/>
+       </div>
+      {/*Profile*/}
+      {tab==='profile' && <DashProfile/>}
+    </div>
   )
 }
 
