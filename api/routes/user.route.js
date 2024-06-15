358141ef11e0b80e8fcaffbd7b16f4cd34808dbe
PiyumalKK <piyumalm912@gmail.com>
2024-06-15 21:26:27 +0530

    Add delete functionality to the admin user

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -9,6 +9,7 @@ router.get('/test', test);
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
-router.get('/getusers',verifyToken, getUsers)
+router.get('/getusers',verifyToken, getUsers);
+
 
 export default router;
--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -54,30 +54,28 @@ export default function DashUsers() {
   }
 
   
-  {/*
+  
     const handleDeleteUser = async () => {
       setShowModal(false);
       try {
-        const res = await fetch(`/api/user/deleteuser/${postIdToDelete}/${currentUser._id}`, {
+        const res = await fetch(`/api/user/delete/${userIdToDelete}`, {
           method: 'DELETE',
         });
         const data = await res.json();
         if (!res.ok) {
           console.log(data.message);
         } else {
-          setUserPosts((prev) =>
-            prev.filter((post) => post._id !== postIdToDelete)
+          setUsers((prev) =>
+            prev.filter((post) => post._id !== userIdToDelete)
           );
+          setShowModal(false);
         }
       } catch (error) {
         console.log(error.message);
       }
     }
     
-    */}
-    const handleDeleteUser = async () => {
-
-    }
+  
 
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 
