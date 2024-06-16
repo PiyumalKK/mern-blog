99e265a752f54c4adbae7360e97ab91bf82e6cf0
PiyumalKK <piyumalm912@gmail.com>
2024-06-16 23:02:17 +0530

    Add search Functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,7 @@ import CreatePost from './pages/CreatePost';
 import UpdatePost from './pages/UpdatePost';
 import PostPage from './pages/PostPage';
 import ScrollToTop from './components/ScrollToTop';
+import Search from './pages/Search';
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
     <Route path="/about" element={<About />} /> 
     <Route path="/sign-in" element={<SignIn />} />
     <Route path="/sign-up" element={<SignUp />} />
+    <Route path="/search" element={<Search />} />
     <Route element = {<PrivateRoute/>}>
     <Route path="/dashboard" element={<Dashboard />} />
     </Route>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,40 +2,55 @@
 //import React from 'react'
 
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon, FaSun } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleTheme } from "../redux/theme/themeSlice";
 import { signoutSuccess } from "../redux/user/userSlice";
-
-
+import { useEffect, useState } from "react";
 
 function Header() {
     const path = useLocation().pathname;
     const dispatch = useDispatch();
     const { currentUser } = useSelector(state => state.user);
     const { theme } = useSelector(state => state.theme);
+    const [searchTerm, setSearchTerm] = useState('');
+    const location = useLocation();
+    const navigate = useNavigate();
+    console.log(searchTerm);
 
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+        if (searchTermFromUrl) {
+            setSearchTerm(searchTermFromUrl);
+        }
+    }, [location.search]);
 
     const handleSignout = async () => {
         try {
             const res = await fetch('/api/user/signout', {
-             method: 'POST',
+                method: 'POST',
             });
             const data = await res.json();
-            if(!res.ok)
-             {
-               console.log(data.message);
-
-             }
-             else{
+            if (!res.ok) {
+                console.log(data.message);
+            } else {
                 dispatch(signoutSuccess());
-             }
+            }
         } catch (error) {
             console.log(error.message);
         }
-  }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('searchTerm', searchTerm);
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`);
+    };
 
     return (
         <Navbar className='border-b-2'>
@@ -43,23 +58,22 @@ function Header() {
                 <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">Piyumals's</span>
                 Blog
             </Link>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <TextInput
                     type='text'
                     placeholder="search...."
                     rightIcon={AiOutlineSearch}
                     className="hidden lg:inline"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
                 />
             </form>
             <Button className="w-12 h-10 lg:hidden" color="gray" pill>
                 <AiOutlineSearch />
             </Button>
             <div className="flex gap-2 md:order-2">
-                <Button className="w-12 h-10 hidden sm:inline" color="gray" pill onClick={()=>dispatch(toggleTheme())}>
-
-{theme === 'light'?<FaSun/> : <FaMoon/>}
-
-                  
+                <Button className="w-12 h-10 hidden sm:inline" color="gray" pill onClick={() => dispatch(toggleTheme())}>
+                    {theme === 'light' ? <FaSun /> : <FaMoon />}
                 </Button>
 
                 {currentUser ? (
@@ -83,10 +97,9 @@ function Header() {
                             </span>
                         </Dropdown.Header>
                         <Link to={'/dashboard?tab=profile'}>
-                          <Dropdown.Item>Profile</Dropdown.Item>
-                        
+                            <Dropdown.Item>Profile</Dropdown.Item>
                         </Link>
-                        <Dropdown.Divider/>
+                        <Dropdown.Divider />
                         <Dropdown.Item onClick={handleSignout}>
                             Sign Out
                         </Dropdown.Item>
