c24db822681800ed2ab51120c5df78ca2fb5aebd
PiyumalKK <piyumalm912@gmail.com>
2024-06-16 14:17:58 +0530

    Add edit functionality to the comments

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -1,58 +1,69 @@
 import Comment from "../models/comment.model.js";
-import { errorHandler } from "../utils/error.js"
+import { errorHandler } from "../utils/error.js";
 
 export const createComment = async (req, res, next) => {
     try {
-        const { content, postId, userId} = req.body;
-        if(userId !== req.user.id)
-            {
-                return next(errorHandler(403,'You are not allowed to create this comment'));
-
-            }
-            const newComment = new Comment({
-                content,
-                postId,
-                userId,
-            });
-            await newComment.save();
-            res.status(200).json(newComment);
+        const { content, postId, userId } = req.body;
+        if (userId !== req.user.id) {
+            return next(errorHandler(403, 'You are not allowed to create this comment'));
+        }
+        const newComment = new Comment({
+            content,
+            postId,
+            userId,
+        });
+        await newComment.save();
+        res.status(200).json(newComment);
     } catch (error) {
         next(error);
     }
 };
 
-export const getPostComments = async (req, res, next) =>{
+export const getPostComments = async (req, res, next) => {
     try {
-        const comments = await Comment.find({postId: req.params.postId}).sort({
+        const comments = await Comment.find({ postId: req.params.postId }).sort({
             createdAt: -1,
         });
         res.status(200).json(comments);
     } catch (error) {
-       next(error); 
+        next(error);
     }
 };
 
 export const likeComment = async (req, res, next) => {
-  try {
-    const comment = await Comment.findById(req.params.commentId);
-    if(!comment)
-        {
-            return next(errorHandler(404, ' Comment not found'));
+    try {
+        const comment = await Comment.findById(req.params.commentId);
+        if (!comment) {
+            return next(errorHandler(404, 'Comment not found'));
         }
-    const userIndex = comment.likes.indexOf(req.user.id);
-    if(userIndex === -1)
-        {
+        const userIndex = comment.likes.indexOf(req.user.id);
+        if (userIndex === -1) {
             comment.numberOfLikes += 1;
             comment.likes.push(req.user.id);
-            
-        }
-        else{
+        } else {
             comment.numberOfLikes -= 1;
             comment.likes.splice(userIndex, 1);
         }
         await comment.save();
         res.status(200).json(comment);
-  } catch (error) {
-    next(error);
-  }
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const editComment = async (req, res, next) => {
+    try {
+        const comment = await Comment.findById(req.params.commentId);
+        if (!comment) {
+            return next(errorHandler(404, 'Comment not found'));
+        }
+        if (comment.userId !== req.user.id && !req.user.isAdmin) {
+            return next(errorHandler(403, 'You are not allowed to edit this comment'));
+        }
+        comment.content = req.body.content;
+        await comment.save();
+        res.status(200).json(comment);
+    } catch (error) {
+        next(error);
+    }
 };
